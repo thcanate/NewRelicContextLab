e410f95183832236d4e8f0c093e856068d7ab099
thcanate <thiago.anate@outlook.com>
2022-10-25 18:08:59 -0300

    Create logger.js

---
+++ answers/FoodMe/logger.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const userlogger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.json()
+    ),
+    transports: [
+            new winston.transports.Console(),
+            new winston.transports.File({ filename: 'applog.log' }),
+    ],
+});
+module.exports = userlogger;
